883126ccb61aa454f60167edc123f1ce70848625
Igor Morozov <24278723+morozoffnor@users.noreply.github.com>
2024-01-07 04:00:46 +0300

    Code formatting for tools files

--- app/tools/logger.mjs
+++ app/tools/logger.mjs
@@ -1,18 +1,18 @@
 import winston from "winston";
 
 export const logger = winston.createLogger({
-  level: "info",
-  format: winston.format.combine(
-    winston.format.timestamp(), // adds a timestamp property
-    winston.format.json(),
-    winston.format.errors({ stack: true }),
-    winston.format.prettyPrint(),
-    winston.format.colorize({ all: true })
-  ),
-  transports: [
-      new winston.transports.Console(),
-      new winston.transports.File({ filename: "error.log", level: "warn" }),
-      new winston.transports.File({ filename: "app.log" })
-  ],
-  handleExceptions: true
+    level: "info",
+    format: winston.format.combine(
+      winston.format.timestamp(), // adds a timestamp property
+      winston.format.json(),
+      winston.format.errors({stack: true}),
+      winston.format.prettyPrint(),
+      winston.format.colorize({all: true})
+    ),
+    transports: [
+        new winston.transports.Console(),
+        new winston.transports.File({filename: "error.log", level: "warn"}),
+        new winston.transports.File({filename: "app.log"})
+    ],
+    handleExceptions: true
 });
--- app/tools/sendPatchnotes.mjs
+++ app/tools/sendPatchnotes.mjs
@@ -1,24 +1,22 @@
 import {bot} from "../main.mjs";
 import {config} from "../config.mjs";
-import pkg from "showdown"
-import {logger} from "./logger.mjs";
-const showdown = pkg
+
 
 export async function sendPatchnotes(release) {
-  let body = getHeaderString(release) + toEscapeMSg(release['body'].toString())
-  await bot.telegram.sendMessage(config.chatId, `${body}`, {parse_mode: "Markdown"})
+    let body = getHeaderString(release) + toEscapeMSg(release['body'].toString())
+    await bot.telegram.sendMessage(config.chatId, `${body}`, {parse_mode: "Markdown"})
 }
 
 function toEscapeMSg(str) {
-  return str
-    .replace("^#+\s+(.+?)\s*$", "**")
-    .replace("^\\s*=+\\s*$", "")
-    .replace("^\\s*-+\\s*$", "")
-    .replace(/##/g, "")
-    .replace(/@/g, "")
-    .replace(/\*\*/gm, "*")
+    return str
+      .replace("^#+\s+(.+?)\s*$", "**")
+      .replace("^\\s*=+\\s*$", "")
+      .replace("^\\s*-+\\s*$", "")
+      .replace(/##/g, "")
+      .replace(/@/g, "")
+      .replace(/\*\*/gm, "*")
 }
 
 function getHeaderString(release) {
-  return `* New release: ${release['name']}*\n\n[Github link](${release['html_url']})\n\n`
+    return `* New release: ${release['name']}*\n\n[Github link](${release['html_url']})\n\n`
 }
