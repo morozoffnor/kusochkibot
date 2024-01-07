5fe757a1ba0df60d28d3d27459f66f834753d315
Igor Morozov <24278723+morozoffnor@users.noreply.github.com>
2024-01-07 04:05:59 +0300

    Code formatting for all the rest of the  files

--- app/commands/addName.mjs
+++ app/commands/addName.mjs
@@ -1,4 +1,3 @@
-import console from "console";
 import {createNewName} from "../database/database.mjs";
 import {checkIfNameExists} from "../tools/namesChecker.mjs";
 import {logger} from "../tools/logger.mjs";
@@ -14,31 +13,31 @@ const allDoneMessage = `Запомнил эту хуйню`
 
 
 export async function addName(ctx) {
-  let text = ctx.message.text.split(' ')
-  text.shift()
-  let name = text.join(' ')
-  if (name.length < 1) {
-    await ctx.reply(
-      invalidSyntaxErrorMessage,
-      {reply_to_message_id : ctx.message.message_id, parse_mode: 'HTML'});
-    return
-  } else {
-    const nameExists = await checkIfNameExists(name)
-    if (nameExists) {
-      await ctx.reply(nameAlreadyPresentErrorMessage, {reply_to_message_id : ctx.message.message_id});
+    let text = ctx.message.text.split(' ')
+    text.shift()
+    let name = text.join(' ')
+    if (name.length < 1) {
+        await ctx.reply(
+          invalidSyntaxErrorMessage,
+          {reply_to_message_id: ctx.message.message_id, parse_mode: 'HTML'});
+        
     } else {
-      try {
-        await createNewName({
-          title: name,
-          addedAt: Date.now(),
-          addedBy: ctx.from.id
-        })
-        await ctx.reply(allDoneMessage, {reply_to_message_id : ctx.message.message_id});
-      } catch (e) {
-        logger.error('Error while adding a new name: ',e)
-        const fatalErrorMessage = `Произошла какая-то хуйня и я не смог то, что должен был смочь. НЕ СМОГ Я ЧЕГО ПРИСТАЛИ БЛЯТЬ\n\nВот ошибка для тупого разраба: \n${e}`
-        await ctx.reply(fatalErrorMessage, {reply_to_message_id : ctx.message.message_id});
-      }
+        const nameExists = await checkIfNameExists(name)
+        if (nameExists) {
+            await ctx.reply(nameAlreadyPresentErrorMessage, {reply_to_message_id: ctx.message.message_id});
+        } else {
+            try {
+                await createNewName({
+                    title: name,
+                    addedAt: Date.now(),
+                    addedBy: ctx.from.id
+                })
+                await ctx.reply(allDoneMessage, {reply_to_message_id: ctx.message.message_id});
+            } catch (e) {
+                logger.error('Error while adding a new name: ', e)
+                const fatalErrorMessage = `Произошла какая-то хуйня и я не смог то, что должен был смочь. НЕ СМОГ Я ЧЕГО ПРИСТАЛИ БЛЯТЬ\n\nВот ошибка для тупого разраба: \n${e}`
+                await ctx.reply(fatalErrorMessage, {reply_to_message_id: ctx.message.message_id});
+            }
+        }
     }
-  }
 }
