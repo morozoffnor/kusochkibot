927a9beb2e3466557c88831102993e38b11f5bd5
Igor Morozov <24278723+morozoffnor@users.noreply.github.com>
2024-01-29 18:56:06 +0300

    bolt item refactor

--- app/tools/items/bolt.mjs
+++ app/tools/items/bolt.mjs
@@ -1,96 +1,97 @@
 export class Bolt {
-    common() {
-        return {
-            name: "Болтище",
-            description: "Очень хорошо кладется на все дела любой сложности и срочности. Самый распространенный предмет в быту.",
-            rarity: "Common",
-            type: "debuff",
-            subtype: "addition",
-            effect: {
-                descriptions: ["прибавляет к размеру хуя цели 5см"],
-                alert: "",
-                title: "Использовать предмет можно только на тех, кто уже замерял хуй сегодня. Если у цели уже есть активный предмет, то болт его заменит собой. Но цель так же может использовать предмет, чтобы избавиться от болта.\n\n"
-            },
-            icon: "boltCommon.png",
-            effectInfo: {
-                option1: 5,
-            }
-            
+    
+    name = "Болтище"
+    description = "Очень хорошо кладется на все дела любой сложности и срочности. Самый распространенный предмет в быту."
+    
+    constructor(item) {
+        if (item) {
+            this.rarity = item.rarity
+            this.type = item.type
+            this.subtype = item.subtype
+            this.effect = item.effect
+            this.icon = item.icon
+            this.effectInfo = item.effectInfo
+            this.id = item.id
         }
+        
+    }
+    
+    common() {
+        this.rarity = "Common"
+        this.type = "debuff"
+        this.subtype = "addition"
+        this.effect = {
+            descriptions: ["прибавляет к размеру хуя цели 5см"],
+            alert: "",
+            title: "Использовать предмет можно только на тех, кто уже замерял хуй сегодня. Если у цели уже есть активный предмет, то болт его заменит собой. Но цель так же может использовать предмет, чтобы избавиться от болта.\n\n"
+        }
+        this.icon = "boltCommon.png"
+        this.effectInfo = {
+            option1: 5
+        }
+        
+        return this
     }
     
     uncommon() {
-        return {
-            name: "Болтище",
-            description: "Очень хорошо кладется на все дела любой сложности и срочности. Самый распространенный предмет в быту.",
-            rarity: "Uncommon",
-            type: "debuff",
-            subtype: "multiply",
-            effect: {
-                descriptions: ["умножает следующий хуй цели на 2"],
-                alert: "",
-                title: "Использовать предмет можно только на тех, кто уже замерял хуй сегодня. Если у цели уже есть активный предмет, то болт его заменит собой. Но цель так же может использовать предмет, чтобы избавиться от болта.\n\n"
-            },
-            icon: "boltUncommon.png",
-            effectInfo: {
-                option1: 2,
-                v: 2
-            }
-            
+        this.rarity = "Uncommon"
+        this.type = "debuff"
+        this.subtype = "multiply"
+        this.effect = {
+            descriptions: ["умножает следующий хуй цели на 2"],
+            alert: "",
+            title: "Использовать предмет можно только на тех, кто уже замерял хуй сегодня. Если у цели уже есть активный предмет, то болт его заменит собой. Но цель так же может использовать предмет, чтобы избавиться от болта.\n\n"
         }
+        this.icon = "boltUncommon.png"
+        this.effectInfo = {
+            option1: 2
+        }
+        
+        return this
     }
     
     rare() {
-        return {
-            name: "Болтище",
-            description: "Очень хорошо кладется на все дела любой сложности и срочности. Самый распространенный предмет в быту.",
-            rarity: "Rare",
-            type: "debuff",
-            subtype: "addition",
-            effect: {
-                descriptions: ["[current size] + 5"],
-                alert: "",
-                title: "Использовать предмет можно только на тех, кто уже замерял хуй сегодня.\n\n"
-            },
-            icon: "boltRare.png",
-            effectInfo: {
-                option1: 5,
-                v: 2
-            }
-            
+        this.rarity = "Rare"
+        this.type = "debuff"
+        this.subtype = "addition"
+        this.effect = {
+            descriptions: ["[current size] + 5"],
+            alert: "",
+            title: "Использовать предмет можно только на тех, кто уже замерял хуй сегодня.\n\n"
         }
+        this.icon = "boltRare.png"
+        this.effectInfo = {
+            option1: 5,
+            v: 2
+        }
+        
+        return this
     }
     
     legendary() {
-        return {
-            name: "Болтище",
-            description: "Очень хорошо кладется на все дела любой сложности и срочности. Самый распространенный предмет в быту.",
-            rarity: "Legendary",
-            type: "debuff",
-            subtype: "multiply",
-            effect: {
-                descriptions: ["[current size] * 5"],
-                alert: "",
-                title: "Использовать предмет можно только на тех, кто уже замерял хуй сегодня.\n\n"
-            },
-            icon: "boltLegendary.png",
-            effectInfo: {
-                option1: 5,
-                v: 2
-            }
-            
+        this.rarity = "Legendary"
+        this.type = "debuff"
+        this.subtype = "multiply"
+        this.effect = {
+            descriptions: ["[current size] * 5"],
+            alert: "",
+            title: "Использовать предмет можно только на тех, кто уже замерял хуй сегодня.\n\n"
         }
+        this.icon = "boltLegendary.png"
+        this.effectInfo = {
+            option1: 5,
+            v: 2
+        }
+        
+        return this
     }
     
-    async useItem(item, size) {
-        if (item.subtype === 'addition') {
-            return size + item.effectInfo.option1
+    useItem(size) {
+        if (this.subtype === 'addition') {
+            return size + this.effectInfo.option1
         }
-        if (item.subtype === 'multiply') {
-            return (size * item.effectInfo.option1).toFixed(3)
+        if (this.subtype === 'multiply') {
+            return (size * this.effectInfo.option1).toFixed(3)
         }
     }
-    
-    
-    
 }
